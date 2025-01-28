276351bae4f537ff9d1f6a98cb9c1829fbb85b81
samuellouf <SamuelLoufCoder@gmail.com>
2025-01-28 10:18:40 +0100

    Prism Folder, Responses
    Created a prism folder, Added code to log responses (only the immediate ones).

--- script.js
+++ script.js
@@ -8,6 +8,19 @@ const sleep = (ms) => { return new Promise((resolve) => { setTimeout(resolve, ms
 
 // `<pre><code class="language-css">p { color: red }</code></pre>`
 
+function stringify(string){
+  if (string.includes('\'') && string.includes('\"') && string.includes('\`')){
+    return `'${string.replaceAll('\'', '\\\'')}'`;
+  } else if (string.includes('\'')){
+    return `"${string}"`;
+  } else if (string.includes('\"')){
+    return `'${string}'`;
+  } else if (string.includes('\`')){
+    return `'${string}'`;
+  }
+  return `'${string}'`;
+}
+
 function execute(){
   const execute_ = async () => {
     let executed_code = document.createElement('pre');
@@ -23,8 +36,31 @@ function execute(){
     document.querySelector('#input code').innerHTML = '';
     scrollTo(0, window.outerHeight);
     await sleep(1);
-    var result = eval(value);
+    const executeCode = (code) => {
+      var result = eval(code);
+      let response_div = document.createElement('div');
+      response_div.classList.add('response');
+      console.appendChild(response_div);
+      console.appendChild(textarea_div);
+      if (result instanceof Promise) {
+      } else {
+        let response_pre = document.createElement('pre');
+        let response_code = document.createElement('code');
+        response_pre.appendChild(response_code);
+        if (typeof result == 'string'){
+          result = stringify(result);
+        } else if (typeof result == 'function'){
+          result = 'ƒ ' + result;
+        }
+        response_code.innerHTML = result;
+        response_pre.classList.add('language-javascript');
+        console.appendChild(response_pre);
+        Prism.highlightElement(response_pre);
+        response_div.appendChild(response_pre);
+      }
+    }
     // await result;
+    executeCode(value);
   };
   execute_();
 }
